f6ff4eb0eacc9e7f4056327ab5a63e0af0d75fed
firevaam <firevaam@MSI>
2025-01-21 10:40:40 +0100

    Changement expiration 1 jour

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -50,7 +50,7 @@ class Configuration
             'info'                     => 'More information on the <a href=\'https://privatebin.info/\'>project page</a>.',
             'notice'                   => '',
             'languageselection'        => false,
-            'languagedefault'          => '',
+            'fr'          => '',
             'urlshortener'             => '',
             'qrcode'                   => true,
             'icon'                     => 'identicon',
@@ -60,7 +60,7 @@ class Configuration
             'compression'              => 'zlib',
         ),
         'expire' => array(
-            'default' => '1week',
+            '30min' => '1800',
         ),
         'expire_options' => array(
             '5min'   => 300,
